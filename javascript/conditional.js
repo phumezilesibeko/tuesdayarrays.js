917eff5fbb2c1481be3d4b177a6464308d2cf104
phumezilesibeko <sibeko.phumezile@gmail.com>
2026-05-27 11:24:11 +0200

    loops

---
+++ javascript/conditional.js
@@ -0,0 +1,18 @@
+// TODO: Create a variable called score and give it a value between 0 and 100 
+let score = 100;
+// TODO: Create a variable called passingGrade and set it to 60 
+let passingGrade = 60;
+// TODO: Write an if statement to check if score is greater than or equal to passingGrade 
+if (score >= passingGrade) {
+    // TODO: Print "Passed" based on the score 
+    console.log("Passed");
+} else {
+    // TODO: Print "Failed" based on the score 
+    console.log("Failed");
+}
+// TODO: Add another condition to check if the score is perfect (100)
+if (score == "100"
+    
+) {
+    console.log("Perfect score!");
+} 
