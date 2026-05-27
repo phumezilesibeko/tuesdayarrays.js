c79e0f15581dfb5b3bf326182567ec40217d9200
phumezilesibeko <sibeko.phumezile@gmail.com>
2026-05-27 09:42:29 +0200

    arrays

---
+++ javascript/arrays.js
@@ -0,0 +1,15 @@
+// TODO: Create an empty array called colors 
+let colors = [];
+// TODO: Add three colors to the array using push() 
+colors.push("red");
+colors.push("green");
+colors.push("blue");
+// TODO: Create another array called numbers with five numbers 
+let numbers = [2, 4, 6, 8, 10];
+// TODO: Remove the last color from the colors array 
+colors.pop();
+// TODO: Add a new color to the beginning of the colors array 
+colors.unshift("yellow");
+// TODO: Print the length of both arrays 
+console.log(colors.length);
+console.log(numbers.length); 
---
+++ javascript/secondarrays.js
@@ -0,0 +1,10 @@
+let colors = [] ;
+
+colors. push("green");
+colors. push("black");
+colors. push("green");
+
+numbers = [2,4,6,8,10];
+
+console.log("Colors array length:", colors.length);
+console.log("Numbers array length:", numbers.length);
